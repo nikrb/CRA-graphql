cef1b38f0d94b1aaa1948d4f33212b886bf9e3a1
nikrb <nik_rb@yahoo.com>
2018-07-09 00:03:12 +0100

    add simple gql query for posts and display

--- src/App.js
+++ src/App.js
@@ -1,13 +1,61 @@
 import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider, Query } from 'react-apollo';
 
-class App extends Component {
+import gql from 'graphql-tag';
+
+const PostList = () => (
+  <Query
+    query={gql`
+      {
+        posts {
+          _id
+          title
+          content
+        }
+      }
+    `}
+  >
+    {({ loading, error, data }) => {
+      console.log(data);
+      if (loading) return <p>Loading...</p>;
+      if (error) return <p>Error...</p>;
+      return(
+        <ul>
+          {data.posts.map(p => <li key={p._id}>{p.title}</li>)}
+        </ul>
+      );
+    }}
+  </Query>
+);
+
+const client = new ApolloClient({
+  uri: 'http://localhost:3000/graphql',
+});
+
+class App extends React.Component {
   render() {
     return (
-      <div className="App">
-
-      </div>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <h1>Blog</h1>
+        </div>
+        <PostList />
+      </ApolloProvider>
     );
   }
 }
+
+// client.query({
+//   query: gql`
+//     {
+//       posts {
+//         _id
+//         title
+//         content
+//       }
+//     }
+//   `
+// }).then(res => console.log('post:', res));
 
 export default App;
