b039044dc2ca0bfedc4e4ee3cfede5217ee3bcd1
nikrb <nik_rb@yahoo.com>
2018-07-09 20:46:36 +0100

    add post comment excerpt

--- src/App.js
+++ src/App.js
@@ -12,17 +12,40 @@ const PostList = () => (
           _id
           title
           content
+          comments {
+            _id
+            postId
+            content
+          }
         }
       }
     `}
   >
     {({ loading, error, data }) => {
-      console.log(data);
       if (loading) return <p>Loading...</p>;
-      if (error) return <p>Error...</p>;
+      if (error) return <p>Error...{error.messageText}</p>;
       return(
         <ul>
-          {data.posts.map(p => <li key={p._id}>{p.title}</li>)}
+          {data.posts.map(p => {
+              return (
+                <div key={p._id}>
+                  <li>
+                    <h3>{p.title}</h3>
+                    <p>{p.content.slice(0, 24)}</p>
+                  </li>
+                  <ul>
+                    {p.comments.map(
+                      c => (
+                        <li key={c._id}>
+                          {c.content.slice(0, 24)}
+                        </li>
+                      ))
+                    }
+                  </ul>
+                </div>
+              );
+            }
+          )}
         </ul>
       );
     }}
